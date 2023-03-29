2c7657300b9a2832ce610c9f7479ef2045da1eea
Andrei Barari <b.andrei91@yahoo.com>
2023-03-29 13:29:34 +0300

    style and functionality changes to rock, paper, scissors app

--- Rock-Paper-Scissors/app.js
+++ Rock-Paper-Scissors/app.js
@@ -1,17 +1,28 @@
-const choices = document.querySelectorAll("button");
+const options = document.querySelectorAll(".choice");
+const optionsFont = document.querySelectorAll(".fa-solid");
 const playerPoints = document.getElementById("playerScore");
 const computerPoints = document.getElementById("computerScore");
+const reset = document.getElementById("refresh");
+const result = document.getElementById("result");
+const turnOutcome = document.querySelector(".turn-outcome");
+playerChoice = document.getElementById("playerChoice");
+npcChoice = document.getElementById("npcChoice");
 
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
 
 playerPoints.innerHTML = playerScore;
 computerPoints.innerHTML = computerScore;
-result.innerHTML = "Make your choice!";
+result.innerHTML = "Pick!";
 
 function disableChoices() {
-  choices.forEach((choice) => {
-    choice.disabled = true;
+  for (let option of options) {
+    option.disabled = true;
+    option.style.backgroundColor = "#cdd3df";
+  }
+  optionsFont.forEach(el => {
+    el.style.color = "#ff7878";
   });
 }
 
@@ -19,45 +30,66 @@ function playRound(playerSelection) {
   const computerChoice = ["Rock", "Paper", "Scissors"];
 
   computerSelection =
-    computerChoice[
-      Math.floor(Math.random() * computerChoice.length)
-    ].toLowerCase();
+    computerChoice[Math.floor(Math.random() * computerChoice.length)].toLowerCase();
   playerSelection = playerSelection.toLowerCase();
-
   if (playerSelection == computerSelection) {
-    result = "Tie!";
+    res = "Tie!";
   } else if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "paper" && playerSelection == "rock")
   ) {
     computerScore++;
-    result = "Computer wins!";
+    res = "Npc wins!";
   } else {
     playerScore++;
-    result = "Player wins!";
+    res = "Player wins!";
   }
 
   if (playerScore == 5 || computerScore == 5) {
     disableChoices();
-    document.getElementById("refresh").style.visibility = "visible";
+    reset.style.visibility = "visible";
   }
+
+  round++;
+  const roundResult = document.createElement("span");
+  roundResult.classList.add("turn-result");
+  roundResult.textContent = `${round}. Player: ${playerSelection} | Npc: ${computerSelection}`;
+  turnOutcome.appendChild(roundResult);
 
   playerPoints.innerHTML = playerScore;
   computerPoints.innerHTML = computerScore;
-  document.getElementById("result").innerHTML = result;
+  result.innerHTML = res;
 }
 
-document
-  .getElementById("refresh")
-  .addEventListener("click", function newGame() {
-    window.location.reload("Refresh");
-  });
+reset.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  round = 0;
 
-choices.forEach((choice) => {
-  choice.addEventListener("click", function () {
-    playRound(choice.value);
-    document.getElementById("playerChoice").innerHTML = choice.value;
-    document.getElementById("npcChoice").innerHTML = computerSelection;
+  playerPoints.innerHTML = playerScore;
+  computerPoints.innerHTML = computerScore;
+  turnOutcome.innerHTML = "";
+  result.innerHTML = "Pick!";
+  reset.style.visibility = "hidden";
+  computerSelection = "";
+  playerSelection = "";
+  playerChoice.textContent = "";
+  npcChoice.textContent = "";
+
+  for (let option of options) {
+    option.disabled = false;
+    option.style.backgroundColor = "#ecf2ff";
+  }
+  optionsFont.forEach(el => {
+    el.style.color = "#444";
   });
 });
+
+for (let option of options) {
+  option.addEventListener("click", function () {
+    playRound(option.value);
+    playerChoice.textContent = option.value;
+    npcChoice.textContent = computerSelection;
+  });
+}
